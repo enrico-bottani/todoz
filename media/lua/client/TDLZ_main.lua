d7821b3948b046bea3c180b7215518d1ea6f5486
Tarma <enrico-bottani@users.noreply.github.com>
2023-08-06 09:23:58 +0200

    Initial commit

---
+++ media/lua/client/TDLZ_main.lua
@@ -0,0 +1,129 @@
+require 'luautils'
+TDLZ_menu = {}
+require 'UI/TDLZ_window'
+require 'ISUI/ISUIWriteJournal'
+local original_ISUIOnClick = ISUIWriteJournal.onClick;
+
+function ISUIWriteJournal:onClick(button)
+    original_ISUIOnClick(self, button)
+
+	if button.internal == "OK" then
+		-- update todolist
+        
+	end
+end
+function mysplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
+
+
+TDLZ_menu.onCraftHelper = function(items, player, itemMode)
+
+    print("n of items: " .. #items)
+    print("category: " .. tostring(items[1]:getCategory()))
+    print("name: " .. tostring(items[1]:getBookName()))
+    print("pages: " .. items[1]:getPageToWrite())
+    print("-------------------------------")
+    -- local next = items[1]:getCustomPages();
+    -- print("page: " .. items[1]:tostring())
+    -- print("type: " .. type(items[1]))
+    local customPagesHashMap = items[1]:getCustomPages() -- userdata
+    -- print(customPagesHashMap:toString() .. " type: " .. type(customPagesHashMap))
+    local b = transformIntoKahluaTable(customPagesHashMap)
+    local levels = ""
+    for perk, level in pairs(b) do
+        levels = levels .. level
+    end
+    local splitted = mysplit(levels, "\n")
+    -- print(type(splitted))
+    --[[
+    for lineNum, line in pairs(splitted) do
+        -- print(line .. " (" .. type(line) .. ")")
+        for w in string.gmatch(line, "^%[% %]") do
+            print("found: " .. line)
+            UI:nextLine()
+
+            UI:addText("", tostring(line), _, "Left");
+          
+            UI:addTickBox("t1");
+        end
+
+    end
+    
+    UI:saveLayout()
+    --]]
+    TDLZ_UI.toggle();
+    print("Open UI")
+end
+
+function getNotebooks(items)
+    local itemsUsedInRecipes = {}
+    local item
+    -- Go through the items selected (because multiple selections in inventory is possible)
+    for i = 1, #items do
+        if not instanceof(items[i], 'InventoryItem') then
+            item = items[i].items[1]
+        else
+            item = items[i]
+        end
+        -- if item is used in any recipe OR there is a way to create this item - mark item as valid
+        local fullType = item:getFullType()
+        print(fullType)
+        local isNote = fullType == 'Base.Notebook'
+        print("isNote : " .. tostring(isNote))
+        if isNote then
+            table.insert(itemsUsedInRecipes, item)
+        end
+    end
+    return itemsUsedInRecipes;
+end
+
+TDLZ_menu.doCraftHelperMenu = function(player, context, items)
+    local itemsUsedInRecipes = getNotebooks(items);
+    
+    -- print(itemsUsedInRecipes[1]:getName())
+    if type(itemsUsedInRecipes) == 'table' and #itemsUsedInRecipes > 0 then
+        local opt = context:addOption(getText('IGUI_TDLZ_context_onclick'), itemsUsedInRecipes, TDLZ_menu.onCraftHelper,
+            player)
+        opt.iconTexture = getTexture('media/textures/TDLZ_ctx_icon.png')
+    end
+
+end
+
+TDLZ_menu.OnBeginRefresh = function(invSelf)
+    print("OnBeginRefreshInv")
+end
+TDLZ_menu.OnRefreshInventoryWindowContainers = function(invSelf, state)
+    local playerObj = getSpecificPlayer(invSelf.player)
+    if invSelf.onCharacter or playerObj:getVehicle() then
+        -- Ignore character containers, as usual
+        -- Ignore in vehicles
+        return
+    end
+
+    if state == "begin" then
+        return TDLZ_menu.OnBeginRefresh(invSelf)
+    end
+
+end
+ISUIWriteJournal:onClick(button){
+    print("Called")
+}
+local function OnDeviceText(player)
+	-- Your code here
+    print("On device text")
+end
+-- In assenzaa di un trigger + specifico...
+Events.EveryOneMinute.Add(OnDeviceText)
+
+Events.OnFillInventoryObjectContextMenu.Add(TDLZ_menu.doCraftHelperMenu)
+Events.OnRefreshInventoryWindowContainers.Add(TDLZ_menu.OnRefreshInventoryWindowContainers)
+Events.OnCreateUI.Add(TDLZ_menu.OnCreateUI)
---
+++ media/lua/client/UI/TDLZ_window.lua
@@ -0,0 +1,191 @@
+TodoListZManagerUI = ISCollapsableWindow:derive("TodoListZManagerUI")
+
+TDLZ_UI = {}
+--************************************************************************--
+--** TodoListZManagerUI - toggle handler
+--************************************************************************--
+TDLZ_UI = {};
+TDLZ_UI.instance = nil;
+TDLZ_UI.toggle = function()
+    if TDLZ_UI.instance == nil then
+        print("RFM - window not initialized - create new window");
+        TDLZ_UI.instance = TodoListZManagerUI:new();
+    else
+        print("RFM - window exists - handle toggling");
+        TDLZ_UI.instance:actualClose();
+        TDLZ_UI.instance = nil;
+    end
+end
+
+
+--************************************************************************--
+--** TodoListZManagerUI:new
+--**
+--************************************************************************--
+function TodoListZManagerUI:new()
+    local mD = self:loadModData();
+
+    local panelWidth = mD.panelSettings.width;
+    local panelHeight = mD.panelSettings.height;
+    local startingX = mD.panelSettings.x;
+    local startingY = mD.panelSettings.y;
+    local pin = mD.panelSettings.pin;
+
+    local o = {}
+    o = ISCollapsableWindow:new(startingX, startingY, panelWidth, panelHeight);
+    setmetatable(o, self);
+    self.__index = self;
+    o.title = getText("IGUI_TDLZ_window_title");
+    o.pin = pin;
+    o.x = startingX;
+    o.y = startingY;
+    o.width = panelWidth;
+    o.height = panelHeight;
+    o.minimumWidth = 300;
+    o.minimumHeight = 100;
+    o.resizable = true;
+    o.drawFrame = true;
+    o.borderColor = {r=0.4, g=0.4, b=0.4, a=1};
+    o.backgroundColor.a = 0.9;
+    o.moveWithMouse = true;
+
+   -- o.storedChannels = mD.storedChannels;
+    o.renderedChannels = {};
+    -- o.k_isFirstRun = mD.isFirstRun;
+
+    o:initialise();
+    o:addToUIManager();
+
+    -- o:setInfo("test");
+
+    return o;
+end
+
+function TodoListZManagerUI:renderStoredChannels()
+    -- clear rendered channels
+    for _, rc in ipairs(self.renderedChannels) do
+        rc.statusBtn:removeFromUIManager();
+        rc.contentBtn:removeFromUIManager();
+        rc.deleteBtn:removeFromUIManager();
+        rc.prnt:removeChild(rc.statusBtn);
+        rc.prnt:removeChild(rc.contentBtn);
+        rc.prnt:removeChild(rc.deleteBtn);
+    end
+    for i, _ in ipairs(self.renderedChannels) do
+        self.renderedChannels[i] = nil;
+    end
+    self.renderedChannels = {};
+
+    --table.sort(self.storedChannels, function(a, b) return a.Freq < b.Freq end);
+
+    -- insert stored channels
+    --local idx = 0;
+    --for _, channel in ipairs(self.storedChannels) do
+        --table.insert(self.renderedChannels, self.createChannelRow(self, channel, idx));
+        --idx  = idx + 1;
+    --end
+
+    -- save changes
+    self:saveModData();
+end
+
+--************************************************************************--
+--** TodoListZManagerUI - base
+--************************************************************************--
+function TodoListZManagerUI:initialise()
+    ISCollapsableWindow.initialise(self);
+    self:create();
+end
+
+function TodoListZManagerUI:prerender()
+    ISCollapsableWindow.prerender(self);
+end
+
+function TodoListZManagerUI:render()
+    ISCollapsableWindow.render(self);
+end
+
+--************************************************************************--
+--** TodoListZManagerUI - creating
+--************************************************************************--
+function TodoListZManagerUI:create()
+    -- "Toolbar" buttons - Import/Export
+    self.copyButton = ISButton:new(15, 25, 25, 25, getText("UI_KRFM_CopyFromRadio"), self, self.onCopy);
+    self.copyButton.tooltip = getText("UI_KRFM_CopyFromRadio_Tooltip");
+    self.copyButton:initialise();
+    self.copyButton:instantiate();
+    self.copyButton.borderColor = {r = 0.7, g = 0.7, b = 0.7, a = 0.5};
+    self:addChild(self.copyButton);
+
+    self.customButton = ISButton:new(self.copyButton:getRight(), 25, 25, 25, getText("UI_KRFM_Custom"), self, self.onCustomAdd);
+    self.customButton:initialise();
+    self.customButton:instantiate();
+    self.customButton.borderColor = {r = 0.7, g = 0.7, b = 0.7, a = 0.5};
+    self:addChild(self.customButton);
+
+    -- Render rows
+    self:renderStoredChannels();
+end
+
+--************************************************************************--
+--** TodoListZManagerUI - actions and radio data processing
+--************************************************************************--
+function TodoListZManagerUI:close()
+    -- Redirect titlebar close, that way we can handle the instance ourselves
+    TDLZ_UI.toggle();
+end
+
+function TodoListZManagerUI:actualClose()
+	ISCollapsableWindow.close(self);
+    self:saveModData();
+    self:setVisible(false);
+    self:removeFromUIManager();
+end
+
+--************************************************************************--
+--** TodoListZManagerUI - mod data
+--************************************************************************--
+function TodoListZManagerUI:loadModData()
+    local player = getPlayer();
+    if player then
+        local modData = player:getModData()
+        local reset = false;
+        if modData.todoListMod == nil or reset == true then
+            modData.todoListMod = {
+                isFirstRun = true,
+                storedChannels = {},
+                panelSettings = { x = 70, y = 400, width = 400, height = 300, pin=false };
+            }
+        end
+        return modData.todoListMod;
+    end
+    print("ERROR: failed to load player and mod data.");
+    return nil;
+end
+
+function TodoListZManagerUI:saveModData()
+    local player = getPlayer();
+    local modData =  player:getModData()
+    modData.todoListMod.isFirstRun = self.k_isFirstRun;
+    modData.todoListMod.storedChannels = self.storedChannels;
+    modData.todoListMod.panelSettings = {
+        x = self.x;
+        y = self.y;
+        width = self.width;
+        height = self.height;
+        pin = self.pin;
+    };
+    player:transmitModData();
+end
+
+TDLZ_menu.OnCreateUI = function()
+    TDLZ_UI.toggle();
+
+    if TDLZ_UI.instance.k_isFirstRun == false then
+        TDLZ_UI.toggle();
+        return;
+    else
+        TDLZ_UI.instance.k_isFirstRun = false;
+    end
+end
+

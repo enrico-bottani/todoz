4fde68466dbf6abfe6197b0e6a72f7a51deb738d
Tarma <enrico-bottani@users.noreply.github.com>
2023-08-09 17:50:03 +0200

    Mod options integration

--- media/lua/client/TDLZ_main.lua
+++ media/lua/client/TDLZ_main.lua
@@ -83,7 +83,27 @@ TDLZ_Menu.onRefreshInventoryWindowContainers = function(inventorySelfInstance, s
         end
     end
 end
+-- These are the default options.
+local SETTINGS = {
+    options = {
+        box1 = true
+    },
+    names = {
+        box1 = "Use colors"
+    },
+    mod_id = "TDLZ",
+    mod_shortname = "Todo List"
+}
 
+-- Connecting the options to the menu, so user can change them.
+if ModOptions and ModOptions.getInstance then
+    ModOptions:getInstance(SETTINGS)
+end
+
+-- Check actual options at game loading.
+Events.OnGameStart.Add(function()
+    print("checkbox1 = ", SETTINGS.options.box1)
+  end)
 Events.OnFillInventoryObjectContextMenu.Add(TDLZ_Menu.handleShowTodoListContenxtMenu)
 Events.OnRefreshInventoryWindowContainers.Add(TDLZ_Menu.onRefreshInventoryWindowContainers)
 Events.OnCreateUI.Add(TDLZ_ISTodoListTZWindowHandler.create)
